9dbde013af8f80fa0771c7cf2371887faafe8331
Ilya Bayanov <i.p.bayanov@gmail.com>
2021-03-31 17:38:49 +0300

    edited secret_example

--- secret_example.js
+++ secret_example.js
@@ -1,4 +1,4 @@
 module.exports = {
-  TOKEN = 'sdvjkfvjbkwebj4bgveuihiweg8hv4389h43489hvhv93b489hWKDBJVK',
-  GROUP_ID = '93698123'
+  TOKEN: 'sdvjkfvjbkwebj4bgveuihiweg8hv4389h43489hvhv93b489hWKDBJVK', //String
+  GROUP_ID: 93698123 //Number
 };
